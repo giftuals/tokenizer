1efc7c2d2f744bfa4e9d801edf4cebe27e3a8c0c
Coen Coppens <coen@kadolijst.nl>
2020-02-03 22:17:30 +0100

    No longer need the settings file, yay

--- src/Config/settings.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    \Tokenizer\Tokenizer::CLAIM_ISSUER => 'Giftuals',
-    \Tokenizer\Tokenizer::CLAIM_SUBJECT => 'Some link to giftuals.com',
-    \Tokenizer\Tokenizer::CLAIM_AUDIENCE => 'https://giftuals.com',
-    \Tokenizer\Tokenizer::HASH_HMAC_KEY => 'your-secret-key',
-];
